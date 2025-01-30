b93d25aecb64f960bec95457480d059a6d9ef958
Chokladglasyr <lim_ida@live.com>
2025-01-30 11:43:16 +0100

    fix:UNSOLVED

--- PlotTwisters/database/migrations/0001_01_01_000000_create_users_table.php
+++ PlotTwisters/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->boolean('is_admin')->default(false);
             $table->timestamps();
         });
