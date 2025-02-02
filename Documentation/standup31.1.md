16b6f24d9852303b5c851520dfd9d3d34bcd61d4
Chokladglasyr <lim_ida@live.com>
2025-02-02 15:23:52 +0100

    feat: added alot of stuff, should have commited earlier

---
+++ Documentation/standup31.1.md
@@ -0,0 +1,36 @@
+# Standup 
+
+### Sprint - 1
+
+### 31/1
+
+### Närvaro
+[ ] Madde  
+[x] Ida  
+[x] Svea  
+[x] Memoona  
+[x] Gabbe  
+[ ] Anna  
+
+
+### Vad jobbade du med igår?
+Gabbe - byggt om projektet  
+
+Svea - fixade footer
+
+Memoona - created movie controller
+
+Ida - deploya, började på homepage layout
+
+### Vad planerar du att jobba på idag?
+Gabbe - lösa gitignore, jobba med header  
+
+Svea - tar en ny issue
+
+Memoona - create rest of controllers
+
+Ida - homepage layout
+
+
+### Finns det några hinder eller problem som måste åtgärdas?
+
---
+++ resources/views/movies/create.blade.php
@@ -0,0 +1,33 @@
+<div>
+    <h1>make movie entry</h1>
+ 
+    <form action="" method="POST">
+     @csrf
+     <div class="mb-3">
+         <label for="title" class="form-label">Title</label>
+         <input type="text" name="title" class="form-control" required>
+     </div>
+ 
+     <div class="mb-3">
+         <label for="description" class="form-label">Description</label>
+         <textarea name="description" class="form-control" rows="3"></textarea>
+     </div>
+ 
+     <div class="mb-3">
+         <label for="trailer_url" class="form-label">Trailer URL</label>
+         <input type="url" name="trailer_url" class="form-control">
+     </div>
+ 
+     <div class="mb-3">
+         <label for="image_url" class="form-label">Image URL</label>
+         <input type="url" name="image_url" class="form-control">
+     </div>
+ 
+     <div class="mb-3">
+         <label for="director_name" class="form-label">Director</label>
+         <input type="text" name="director_name" class="form-control">
+     </div>
+ 
+     <button type="submit" class="btn btn-primary">Save Movie</button>
+ </form>
+ </div>
