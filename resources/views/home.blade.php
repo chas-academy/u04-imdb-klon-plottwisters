055b20ebabf5cb79a067a31e38ccb8c318ed0081
Chokladglasyr <lim_ida@live.com>
2025-02-03 14:42:18 +0100

    feat: made component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,32 +36,11 @@
                     <option value="3">Comedy</option>
                     <option value="4">Drama</option>
                     <option value="5">Action</option>
+                    
                 </select>
             </form>
         </div>
-        <div class="flex flex-row flex-wrap w-2/3 mx-auto">
-            @foreach ($movies as $movie)
-
-                    <div class="flex flex-col flex-1 items-center m-4">
-                        {{-- <p>{{ $movie['title'] }}</p> --}}
-                        <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-info"><img src="{{ $movie->image_url}}" alt=""></a>
-                        {{-- Add function for rating --}}
-                        <img src="{{ Storage::url('images/star.png')}}" alt=""><p class="text-[#A693FF]">3.1</p>
-                        {{-- Route to store into watchlist, right now its set to home because missing controller for watchlist --}}
-                        <a href="{{ route('home', $movie->id) }}" class="btn btn-info bg-[#20C8A6] w-40 text-center rounded-md font-bold">Add to watchlist</a>
-                    
-                    {{-- @if (Auth::check() && Auth::user()->name == 'admin')
-                    <a class="text-white" href="{{ route('movies.edit', $movie->id) }}" class="btn btn-warning">Edit</a>
-
-                    <form action="{{ route('movies.destroy', $movie->id) }}" method="POST" style="display: inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button class="text-white" type="submit" class="btn btn-delete" onclick="return confirm('Are you sure you want to delete this movie?')">Delete</button>
-                    </form>
-                    @endif --}}
-                </div>
-
-            @endforeach
+        @include ('layouts.movies')
         </div>
         @if (Auth::check() && Auth::user()->name == 'admin')
 
