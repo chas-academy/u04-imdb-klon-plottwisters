80a424ca733844a8393f1b6702bc1f75ed41fb93
Chokladglasyr <lim_ida@live.com>
2025-02-05 10:36:34 +0100

    created movie_genre controller

---
+++ app/Http/Controllers/Movie_genreController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie_genre;
+use Illuminate\Http\Request;
+
+class Movie_genreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Movie_genre $movie_genre)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Movie_genre $movie_genre)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Movie_genre $movie_genre)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Movie_genre $movie_genre)
+    {
+        //
+    }
+}
