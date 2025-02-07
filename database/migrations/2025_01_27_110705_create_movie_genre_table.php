73c611177404188d32a8fa7fc5fa2ea69c2f8a06
Chokladglasyr <lim_ida@live.com>
2025-02-07 11:18:57 +0100

    added timestamps to table movie_genres

--- database/migrations/2025_01_27_110705_create_movie_genre_table.php
+++ database/migrations/2025_01_27_110705_create_movie_genre_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('movie_genres', function (Blueprint $table) {
             $table->foreignId('movie_id')->constrained()->onDelete('cascade');
             $table->foreignId('genre_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
