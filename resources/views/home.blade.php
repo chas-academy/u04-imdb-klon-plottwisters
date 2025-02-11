0420fd93cc21e8ad5b53401c884ddf8aed9e55bf
Chokladglasyr <lim_ida@live.com>
2025-02-11 14:51:41 +0100

    feat:footer added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>PlotTwisters</title>
-
-        <link rel="shortcut icon" type="image/x-icon" href="resources\css\icon\PT.svg">
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
-        <!-- Styles / Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @endif
-    </head>
-    <body class="bg-[#1E1E1E]">
-        @include ('layouts.navigation')
+<x-head-layout>
+        {{-- @include ('layouts.navigation') --}}
         <div class="flex flex-1 items-start w-fit m-4 flex-col mx-auto">
             <h2 class="text-white font-bold text-3xl">Featured movie</h2>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/LNlrGhBpYjc?si=2eNr-AiXiSOwc5xG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -60,4 +40,4 @@
 
 
         {{ $movies->links() }}
-    </body>
+</x-head-layout>
