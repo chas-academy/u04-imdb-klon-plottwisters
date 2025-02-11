8dff91d698455b5818b53d912eb45ad343127b1a
Gabriel Kereklidis Paulin <gabriel.paulin111@gmail.com>
2025-02-11 13:35:36 +0100

    mvp ratings

---
+++ resources/views/components/rating.blade.php
@@ -0,0 +1,31 @@
+<div class="flex flex-col items-center space-y-2">
+    <p class="text-lg font-semibold text-white">Average Rating: {{ round($movie->average_rating, 1) }} / 10</p>
+
+    @auth
+    @php
+    $userRating = ($movie->ratings ?? collect())->where('user_id', auth()->id())->first();
+    @endphp
+
+    <form action="{{ route('ratings.store', $movie) }}" method="POST" class="flex items-center space-x-2">
+        @csrf
+        <select name="rating" class="p-1 border border-gray-500 rounded bg-white text-black">
+            @for ($i = 1; $i <= 10; $i++)
+                <option value="{{ $i }}" {{ $userRating && $userRating->rating == $i ? 'selected' : '' }}>
+                {{ $i }}
+                </option>
+                @endfor
+        </select>
+        <x-primary-button>
+            Submit
+        </x-primary-button>
+    </form>
+
+
+
+    @if ($userRating)
+    <p class="text-sm text-gray-400">Your Rating: {{ $userRating->rating }} / 10</p>
+    @endif
+    @else
+    <p class="text-sm text-gray-400">Sign in to rate</p>
+    @endauth
+</div>
--- resources/views/layouts/movies.blade.php
+++ resources/views/layouts/movies.blade.php
@@ -1,28 +1,31 @@
 <div class="flex flex-row flex-wrap w-2/3 mx-auto gap-8">
     @foreach ($movies as $movie)
-        <div class="flex flex-col flex-1 items-center gap-2">
-            {{-- <p>{{ $movie['title'] }}</p> --}}
-            <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-info">
-                <img class="w-[100px] h-[150px]" src="{{ $movie->image_url}}" alt="">
-            </a>
-            {{-- Add function for rating --}}
-            <div class="flex flex-row items-center gap-2">
-                <img src="{{ Storage::url('images/star.png')}}" alt="">
-                <p class="text-[#A693FF]">3.1</p>
-            </div>
-            {{-- Route to store into watchlist, right now it's set to home because missing controller for watchlist --}}
+    <div class="flex flex-col flex-1 items-center gap-2">
+        {{-- <p>{{ $movie['title'] }}</p> --}}
+        <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-info">
+            <img class="w-[100px] h-[150px]" src="{{ $movie->image_url}}" alt="">
+        </a>
+        {{-- Add function for rating --}}
+        <div class="flex flex-row items-center gap-2">
+            <img src="{{ Storage::url('images/star.png')}}" alt="">
+            <p class="text-[#A693FF]">3.1</p>
+        </div>
+        {{-- Route to store into watchlist, right now it's set to home because missing controller for watchlist --}}
 
-            <a class="inline-flex items-center w-xl px-3 py-2 bg-teal-700 border border-transparent rounded-xl font-semibold text-xs text-black uppercase w-[150px]" href="{{ route('movies.edit', $movie->id) }}" class="btn btn-warning ">add to watchlist</a>
+        <a class="inline-flex items-center w-xl px-3 py-2 bg-teal-700 border border-transparent rounded-xl font-semibold text-xs text-black uppercase w-[150px]" href="{{ route('movies.edit', $movie->id) }}" class="btn btn-warning ">add to watchlist</a>
 
-            {{-- @if (Auth::check() && Auth::user()->name == 'admin')
+        {{-- @if (Auth::check() && Auth::user()->name == 'admin')
             <a class="text-white" href="{{ route('movies.edit', $movie->id) }}" class="btn btn-warning">Edit</a>
 
-            <form action="{{ route('movies.destroy', $movie->id) }}" method="POST" style="display: inline;">
-                @csrf
-                @method('DELETE')
-                <button class="text-white" type="submit" class="btn btn-delete" onclick="return confirm('Are you sure you want to delete this movie?')">Delete</button>
-            </form>
-            @endif --}}
-        </div>
+        <form action="{{ route('movies.destroy', $movie->id) }}" method="POST" style="display: inline;">
+            @csrf
+            @method('DELETE')
+            <button class="text-white" type="submit" class="btn btn-delete" onclick="return confirm('Are you sure you want to delete this movie?')">Delete</button>
+        </form>
+        @endif --}}
+
+        @include('components.rating', ['movie'=>$movie])
+
+    </div>
     @endforeach
 </div>
