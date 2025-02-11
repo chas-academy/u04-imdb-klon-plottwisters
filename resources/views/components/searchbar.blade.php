20b76af79e65ed050465a8a8057f09273efbfc6f
Chokladglasyr <lim_ida@live.com>
2025-02-11 16:18:59 +0100

    style:changed a little to searchbar

--- resources/views/components/searchbar.blade.php
+++ resources/views/components/searchbar.blade.php
@@ -1,11 +1,12 @@
-<div class="flex justify-center py-2">
+<div class="flex justify-center">
     <form action="{{ url('/search') }}" method="GET" class="flex">
         <input
             type="text"
             name="search"
             placeholder="Search movies..."
-            class="px-2 py-1 border rounded text-gray-700 focus:outline-none"
+            class="px-2 py-1 border rounded-xl text-gray-700 focus:outline-none mr-2 w-96"
             value="{{ request('search') }}">
-        <button type="submit" class="ml-2 px-3 py-1 bg-blue-500 text-white rounded">Search</button>
+        {{-- <button type="submit" class="ml-2 px-3 py-1 bg-blue-500 text-white rounded">Search</button> --}}
+        <x-primary-button>Search</x-primary-button>
     </form>
 </div>
