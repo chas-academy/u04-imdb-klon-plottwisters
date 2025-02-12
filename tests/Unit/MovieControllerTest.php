a4c9bfde2d248db524f87eba3b8633e58a4c6d8d
memoona.tahira <memoona.tahira@gmail.com>
2025-02-12 10:41:50 +0100

    complete tests for CRUD

--- tests/Unit/MovieControllerTest.php
+++ tests/Unit/MovieControllerTest.php
@@ -15,22 +15,21 @@ class MovieControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-
-public function test_index_displays_movies()
+    public function test_index_displays_movies()
     {
-
         $user = User::factory()->create();
-         $genre1 = Genre::factory()->create(['genre_name' => 'Action']);
-         $genre2 = Genre::factory()->create(['genre_name' => 'Comedy']);
-        // Create some movies
-         $movie1 = Movie::factory()->create([
-                     'title' => 'Test Movie 1',
-                     'image_url' => 'https://example.com/image1.jpg'
-                 ]);
-                 $movie2 = Movie::factory()->create([
-                     'title' => 'Test Movie 2',
-                     'image_url' => 'https://example.com/image2.jpg'
-                 ]);
+        $genre1 = Genre::factory()->create(['genre_name' => 'Action']);
+        $genre2 = Genre::factory()->create(['genre_name' => 'Comedy']);
+
+        $movie1 = Movie::factory()->create([
+            'title' => 'Test Movie 1',
+            'image_url' => 'https://example.com/image1.jpg'
+        ]);
+
+        $movie2 = Movie::factory()->create([
+            'title' => 'Test Movie 2',
+            'image_url' => 'https://example.com/image2.jpg'
+        ]);
 
         $response = $this->actingAs($user)
             ->get('/movies');
@@ -38,28 +37,73 @@ public function test_index_displays_movies()
         $response->assertStatus(200);
         $response->assertViewHas('movies');
         $response->assertViewHas('genres');
-        $response->assertSee($movie1->image_url) ;
+        $response->assertSee($movie1->image_url);
         $response->assertSee($movie2->image_url);
     }
 
     public function test_store_creates_movie()
-        {
-            // Create and authenticate a user
-            $user = User::factory()->create();
+    {
+        $user = User::factory()->create();
 
-            $movieData = [
-                'title' => 'Test Movie',
-                'description' => 'This is a test description.',
-                'trailer_url' => 'https://example.com/trailer.mp4',
-                'image_url' => 'https://example.com/image.jpg',
-                'director_name' => 'John Doe',
-            ];
+        $movieData = [
+            'title' => 'Test Movie',
+            'description' => 'This is a test description.',
+            'trailer_url' => 'https://example.com/trailer.mp4',
+            'image_url' => 'https://example.com/image.jpg',
+            'director_name' => 'John Doe',
+        ];
 
-            $response = $this->actingAs($user)
-                ->post('/movies', $movieData);
+        $response = $this->actingAs($user)
+            ->post('/movies', $movieData);
 
-            $response->assertRedirect();
-            $this->assertDatabaseHas('movies', $movieData);
-        }
+        $response->assertRedirect();
+        $this->assertDatabaseHas('movies', $movieData);
+    }
 
+    public function test_update_modifies_movie()
+    {
+        $user = User::factory()->create();
+        $movie = Movie::factory()->create();
+
+        $updatedData = [
+            'title' => 'Updated Movie Title',
+            'description' => 'Updated description',
+            'trailer_url' => 'https://example.com/updated-trailer.mp4',
+            'image_url' => 'https://example.com/updated-image.jpg',
+            'director_name' => 'Jane Smith',
+        ];
+
+        $response = $this->actingAs($user)
+            ->put("/movies/{$movie->id}", $updatedData);
+
+        $response->assertRedirect();
+        $this->assertDatabaseHas('movies', $updatedData);
+        $this->assertDatabaseMissing('movies', [
+            'id' => $movie->id,
+            'title' => $movie->title
+        ]);
+    }
+
+    public function test_destroy_deletes_movie()
+    {
+        $user = User::factory()->create();
+        $movie = Movie::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->delete("/movies/{$movie->id}");
+
+        $response->assertRedirect();
+        $this->assertDatabaseMissing('movies', ['id' => $movie->id]);
+    }
+
+    public function test_unauthenticated_user_cannot_access_movies()
+    {
+        // Test index
+        $this->get('/movies')->assertRedirect('/login');
+
+        $movie = Movie::factory()->create();
+
+        // Test show
+        $this->get("/movies/{$movie->id}")->assertRedirect('/login');
+    }
 }
