3510f801649b4cc806df8e937f567c7ac258af7c
Chokladglasyr <lim_ida@live.com>
2025-02-13 14:45:50 +0100

    added gabbes things

--- database/migrations/2025_01_24_103141_create_ratings_table.php
+++ database/migrations/2025_01_24_103141_create_ratings_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->integer('rating');
             $table->foreignId('movie_id')->constrained('movies')->onDelete('cascade');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
