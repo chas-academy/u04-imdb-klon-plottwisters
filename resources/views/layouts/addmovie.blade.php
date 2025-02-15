991567baffe61cabcded0ccdce1726314bceb12b
Chokladglasyr <lim_ida@live.com>
2025-02-15 22:08:12 +0100

    style:fixed styling to center delete button

--- resources/views/layouts/addmovie.blade.php
+++ resources/views/layouts/addmovie.blade.php
@@ -10,31 +10,30 @@
         @endif
 <div class="w-2/4 mx-auto absolute inset-0 top-20 px-8 bg-gray-200 h-fit pb-8">
     <a class="float-right p-4 " href="{{ route('home') }}">X</a>
-        <form class="flex flex-col gap-2 mt-4 pl-12" action="@if (request()->has('edit')) {{ route('movies.update', $movie->id) }} @else {{ route('movies.store')}} @endif" method="POST">
-            @csrf
-            @if (request()->has('edit'))
-            @method('PUT')
-            @endif
-            <label>Movie trailer url:</label>
-            <input type="text" value="@if (request()->has('edit')) {{ $movie->trailer_url }} @endif" name="trailer_url">
-            <label>Movie image url:</label>
-            <input type="text" value="@if (request()->has('edit')) {{ $movie->image_url }} @endif"name="image_url">
-            <label>Title:</label>
-            <input type="text" name="title" value="@if (request()->has('edit')) {{ $movie->title }} @endif" required>
-            <label>Director name:</label>
-            <input type="text" value="@if (request()->has('edit')) {{ $movie->director_name }} @endif" name="director_name" required>
-            <label>Description</label>
-            <textarea rows="15" name="description" required>
-                @if (request()->has('edit')) {{ $movie->description }} @endif
-            </textarea>
-            <button class="btn btn-info bg-[#20C8A6] text-center rounded-md font-bold w-1/3 mx-auto" type="submit">Save</button>
-        </form>
+    <form class="flex flex-col gap-2 mt-4 pl-12" action="@if (request()->has('edit')) {{ route('movies.update', $movie->id) }} @else {{ route('movies.store')}} @endif" method="POST">
+        @csrf
         @if (request()->has('edit'))
-            <form class="p-20" action="{{ route('movies.destroy', [$movie->id, 'delete'])}}" method="POST">
-                @csrf
-                @method ('DELETE')
-                <button class="btn btn-info bg-[#F15C5F] text-center rounded-md font-bold w-1/3 mx-auto mt-4" type="submit" value="delete">Delete</button>
-            </form>
+        @method('PUT')
         @endif
-    </div>
+        <label>Movie trailer url:</label>
+        <input type="text" value="@if (request()->has('edit')) {{ $movie->trailer_url }} @endif" name="trailer_url">
+        <label>Movie image url:</label>
+        <input type="text" value="@if (request()->has('edit')) {{ $movie->image_url }} @endif"name="image_url">
+        <label>Title:</label>
+        <input type="text" name="title" value="@if (request()->has('edit')) {{ $movie->title }} @endif" required>
+        <label>Director name:</label>
+        <input type="text" value="@if (request()->has('edit')) {{ $movie->director_name }} @endif" name="director_name" required>
+        <label>Description</label>
+        <textarea rows="15" name="description" required>
+            @if (request()->has('edit')) {{ $movie->description }} @endif
+        </textarea>
+        <button class="btn btn-info bg-[#20C8A6] text-center rounded-md font-bold w-1/3 mx-auto" type="submit">Save</button>
+    </form>
+    @if (request()->has('edit'))
+        <form class="flex justify-center pb-20" action="{{ route('movies.destroy', [$movie->id, 'delete'])}}" method="POST">
+            @csrf
+            @method ('DELETE')
+            <button class="btn btn-info bg-[#F15C5F] text-center rounded-md font-bold w-1/4 mx-auto mt-4" type="submit" value="delete">Delete</button>
+        </form>
+    @endif  
 </div>
