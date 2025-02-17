6429016906ed37f86bcc3a4738ada06c574e2674
Chokladglasyr <lim_ida@live.com>
2025-02-17 11:36:57 +0100

    style: responsiveness leave a review

--- resources/views/layouts/review_create.blade.php
+++ resources/views/layouts/review_create.blade.php
@@ -7,10 +7,11 @@
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @endif
-<div class="w-3/4 lg:w-2/4 mx-auto absolute inset-0 top-16 px-8 bg-gray-200 h-fit pb-8">
-    <div class="mt-8 flex place-content-between">
-        <img class ="w-[100px] h-[150px]" src="{{$movie->image_url}}" alt="">
-        <a href="{{ route('movies.show', $movie->id) }}">X</a>
+<div class="w-3/4 lg:w-2/4 mx-auto absolute inset-0 top-20 px-8 bg-gray-200 h-fit pb-8">
+    <div class="mt-8">
+        <a class="flex justify-end" href="{{ route('movies.show', $movie->id) }}">X</a>
+        <img class ="hidden sm:flex sm:w-[100px] sm:h-[150px]" src="{{$movie->image_url}}" alt="">
+
     </div>
         <form class="flex flex-col gap-4" action="@if (request()->has('reviewEdit')) {{ route('review.update', ['id' => $review->id]) }} @else {{ route('reviews', $movie->id)}} @endif" method="POST">
             @csrf
